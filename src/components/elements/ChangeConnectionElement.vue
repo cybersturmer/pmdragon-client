11ce5fcf2a19fb53dae17c4c8ce4043283399f12
Vladimir Shturmer <cybersturmer@ya.ru>
2020-12-30 20:00:51 +0300

    New floating element to choose a host.

---
+++ src/components/elements/ChangeConnectionElement.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page-sticky position="top-right" :offset="[50, 50]">
+    <q-fab
+      icon="settings"
+      direction="left"
+      color="primary">
+      <q-fab-action
+        label="Set custom host"
+        color="primary"
+        icon="electrical_services" />
+    </q-fab>
+  </q-page-sticky>
+</template>
+
+<script>
+export default {
+  name: 'ChangeConnectionElement'
+}
+</script>
+
+<style scoped>
+
+</style>
