9ffcb0f50cf21d40af35ec177f26870e9bd6364a
Vladimir Shturmer <cybersturmer@ya.ru>
2021-01-01 16:44:38 +0300

    Chart Elements in Components.

---
+++ src/components/charts/BurnDownChart.vue
@@ -0,0 +1,28 @@
+<template>
+
+</template>
+
+<script>
+import LineChart from 'components/charts/LineChart.vue'
+
+export default {
+  name: 'BurnDownChart',
+  components: { LineChart },
+  data: () => ({
+    loaded: false,
+    chartdata: null
+  }),
+  async mounted () {
+    this.loaded = false
+    try {
+    //  Dispatch method to get history
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
