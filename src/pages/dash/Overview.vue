74ffe0084c139b61e7317e89d3130251a6517a3e
Vladimir Shturmer <cybersturmer@ya.ru>
2021-01-01 16:45:03 +0300

    Overview circular progress.

--- src/pages/dash/Overview.vue
+++ src/pages/dash/Overview.vue
@@ -1,12 +1,28 @@
 <template>
   <q-page class="q-pa-lg">
-    Overview page
+    <q-circular-progress
+      show-value
+      size="50px"
+      min="0"
+      max="100"
+      :value="sprintDonePercentage"
+      center-color="primary"
+      color="accent">
+      {{ sprintDonePercentage }}%
+    </q-circular-progress>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'Overview'
+  name: 'Overview',
+  computed: {
+    sprintDonePercentage () {
+      const totalSP = this.$store.getters['issues/STORY_POINT_TOTAL_FOR_STARTED_SPRINT']
+      const currentSPDone = this.$store.getters['issues/STORY_POINT_DONE_FOR_STARTED_SPRINT']
+      return Math.round(currentSPDone / totalSP * 100)
+    }
+  }
 }
 </script>
 
