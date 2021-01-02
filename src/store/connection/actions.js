3feba987d95f69a58158b426349b1541e1aaa8b2
Vladimir Shturmer <cybersturmer@ya.ru>
2021-01-02 16:30:01 +0300

    No need to validate url before committing to Vuex  storage.

--- src/store/connection/actions.js
+++ src/store/connection/actions.js
@@ -1,7 +1,4 @@
-import { isURLValid } from 'src/services/util'
-
-export async function UPDATE_CONNECTION ({ commit }, payload) {
-  if (!isURLValid(payload)) return false
+export function UPDATE_HOST ({ commit }, payload) {
   commit('SET_HOST', payload)
 }
 
