b44061f34c9132b9eac3ead07c8be6de62b3ae04
Vladimir Shturmer <cybersturmer@ya.ru>
2021-01-10 22:21:27 +0300

    field Validation changed to modules.

--- src/pages/mixins/fieldValidation.js
+++ src/pages/mixins/fieldValidation.js
@@ -7,11 +7,11 @@ export const fieldValidationMixin = {
         return false
       }
     },
-    isFieldValid (field) {
-      return this.formErrors[field].length > 0
+    isFieldValid (module, field) {
+      return this[module][field].length > 0
     },
-    resetFieldErrorMessage (field) {
-      this.formErrors[field] = ''
+    resetFieldErrorMessage (module, field) {
+      this[module][field] = ''
     },
     isValidEmail (emailString) {
       const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/
