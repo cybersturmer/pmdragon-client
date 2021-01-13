6ccc5c8cc00e60e9f0f216c0a83c9efcfaea7a99
Vladimir Shturmer <cybersturmer@ya.ru>
2021-01-14 00:58:33 +0300

    Me unified and now looks like Settings.

--- src/pages/dash/Me.vue
+++ src/pages/dash/Me.vue
@@ -1,130 +1,128 @@
 <template>
   <q-page class="flex q-layout-padding">
-    <div class="column full-width">
-      <div class="row">
-        <!-- Main user Data card -->
-        <!-- @todo Better to move it to separate component -->
-        <q-card dark bordered class="bg-grey-9 q-ma-sm">
-          <q-card-section class="me_card">
-            <div class="text-h6 text-center">Main user Data</div>
-            <q-input
-              dark
-              dense
-              square
-              outlined
-              type="text"
-              v-model="userFormData.firstName"
-              label="First name"
-              class="q-mb-sm"
-              standout="text-white bg-primary"
-            />
-            <q-input
-              dark
-              dense
-              square
-              outlined
-              type="text"
-              v-model="userFormData.lastName"
-              label="Last name"
-              class="q-mb-sm"
-              standout="text-white bg-primary"
-            />
-            <q-input
-              dark
-              dense
-              square
-              outlined
-              type="text"
-              v-model="userFormData.userName"
-              label="Username"
-              standout="text-white bg-primary"
-            />
-          </q-card-section>
-
-          <q-card-actions vertical>
-            <q-btn flat @click="saveUserData">
-              Update
-            </q-btn>
-          </q-card-actions>
-        </q-card>
-
-        <!-- Password card -->
-        <!-- @todo Better to move it to separate component -->
-        <q-card dark bordered class="bg-grey-9 q-ma-sm">
-          <q-card-section class="me_card">
-            <div class="text-h6 text-center">Password</div>
-            <q-input
-              dark
-              dense
-              square
-              outlined
-              type="password"
-              label="Old password"
-              v-model="passwordFormData.oldPassword"
-              class="q-mb-sm"
-              standout="text-white bg-primary"
-            />
-            <q-input
-              dark
-              dense
-              square
-              outlined
-              type="password"
-              label="Password"
-              v-model="passwordFormData.newPassword1"
-              class="q-mb-sm"
-              standout="text-white bg-primary"
-            />
-            <q-input
-              dark
-              dense
-              square
-              outlined
-              type="password"
-              label="Password confirmation"
-              v-model="passwordFormData.newPassword2"
-              standout="text-white bg-primary"
-            />
-          </q-card-section>
-
-          <q-card-actions vertical>
-            <q-btn flat @click="savePassword">
-              Update
-            </q-btn>
-          </q-card-actions>
-        </q-card>
-
-        <!-- Avatar card -->
-        <!-- @todo Better to move it to separate component -->
-        <q-card dark bordered class="bg-grey-9 q-ma-sm">
-          <q-card-section class="me_card">
-            <div class="text-h6 text-center">Avatar picture</div>
-            <q-uploader
-              dark
-              square
-              flat
-              :accept="avatarAllowMimes"
-              max-files="1"
-              :factory="uploadFileAvatar"
-              auto-upload
-              hide-upload-btn
-              @removed="onRemoved"
-              style="max-width: 180px"
-            />
-          </q-card-section>
-        </q-card>
-      </div>
+    <div class="full-width">
+      <q-tabs
+        class="bg-secondary"
+        v-model="tab"
+        dense
+        align="justify"
+        narrow-indicator>
+        <q-tab name="general" icon="stars" label="General"/>
+        <q-tab name="security" icon="security" label="Security"/>
+      </q-tabs>
+      <q-separator dark/>
+      <q-tab-panels
+        class="bg-primary"
+        style="border: 1px solid #777"
+        v-model="tab"
+        animated
+        transition-next="fade"
+        transition-prev="fade">
+        <q-tab-panel name="general">
+          <!-- Main user Data card -->
+          <!-- @todo Better to move it to separate component -->
+          <SettingPanelCard>
+              <template #section>
+                <q-card-section horizontal>
+                    <q-card-section class="col-md-9">
+                      <q-input
+                        dark
+                        flat
+                        type="text"
+                        color="amber"
+                        v-model="userFormData.firstName"
+                        label="First name"
+                      />
+                      <q-input
+                        dark
+                        flat
+                        type="text"
+                        color="amber"
+                        v-model="userFormData.lastName"
+                        label="Last name"
+                      />
+                      <q-input
+                        dark
+                        flat
+                        type="text"
+                        color="amber"
+                        v-model="userFormData.userName"
+                        label="Username"
+                      />
+                    </q-card-section>
+                    <q-separator dark vertical />
+                    <q-card-section class="col-auto">
+                      <q-uploader
+                        dark
+                        flat
+                        :accept="avatarAllowMimes"
+                        max-files="1"
+                        :factory="uploadFileAvatar"
+                        auto-upload
+                        hide-upload-btn
+                        @removed="onRemoved"
+                        style="max-width: 180px"
+                      />
+                    </q-card-section>
+                  </q-card-section>
+              </template>
+            </SettingPanelCard>
+        </q-tab-panel>
+        <q-tab-panel name="security">
+          <SettingPanelCard>
+            <!-- Password card -->
+            <!-- @todo Better to move it to separate component -->
+            <template #section>
+              <q-input
+                dark
+                dense
+                square
+                outlined
+                type="password"
+                label="Old password"
+                v-model="passwordFormData.oldPassword"
+                class="q-mb-sm"
+                standout="text-white bg-primary"
+              />
+              <q-input
+                dark
+                dense
+                square
+                outlined
+                type="password"
+                label="Password"
+                v-model="passwordFormData.newPassword1"
+                class="q-mb-sm"
+                standout="text-white bg-primary"
+              />
+              <q-input
+                dark
+                dense
+                square
+                outlined
+                type="password"
+                label="Password confirmation"
+                v-model="passwordFormData.newPassword2"
+                standout="text-white bg-primary"
+              />
+            </template>
+          </SettingPanelCard>
+        </q-tab-panel>
+      </q-tab-panels>
     </div>
   </q-page>
 </template>
 
 <script>
 import { AVATAR_ALLOW_MIMES } from 'src/services/allow'
+import SettingPanelCard from 'components/elements/SettingPanelCard'
 
 export default {
   name: 'AccountView',
+  components: { SettingPanelCard },
   data () {
     return {
+      tab: 'general',
       userFormData: {
         firstName: this.$store.getters['auth/MY_FIRST_NAME'],
         lastName: this.$store.getters['auth/MY_LAST_NAME'],
