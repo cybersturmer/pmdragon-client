d28848e9b10a0dd6dfba782fd872b5e3655d0d26
Vladimir Shturmer <cybersturmer@ya.ru>
2021-02-05 01:44:22 +0300

    Small server created.

---
+++ server.js
@@ -0,0 +1,12 @@
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.ENV.PORT || 5000
+
+const app = express()
+const path = require('path')
+
+app.use(history())
+app.use(serveStatic(path.join(__dirname, '/dist/spa')))
+app.listen(port)
