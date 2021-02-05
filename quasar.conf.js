04b41249a67ffe97d4806145db583e9ba477356c
Vladimir Shturmer <cybersturmer@ya.ru>
2021-02-06 02:41:16 +0300

    command updated.

--- quasar.conf.js
+++ quasar.conf.js
@@ -74,11 +74,11 @@ module.exports = function (/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
-      https: {
-        key: fs.readFileSync('ssl/localhost-key.pem'),
-        cert: fs.readFileSync('ssl/localhost.pem'),
-        ca: fs.readFileSync('ssl/mkcert development CA 209615209350349705268104912190401250134.crt')
-      },
+      // https: {
+      //   key: fs.readFileSync('ssl/localhost-key.pem'),
+      //   cert: fs.readFileSync('ssl/localhost.pem'),
+      //   ca: fs.readFileSync('ssl/mkcert development CA 209615209350349705268104912190401250134.crt')
+      // },
       port: 8080,
       open: false
     },
