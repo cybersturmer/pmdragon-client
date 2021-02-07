230cd4c8c191371f5dd7efcaebfae7c13b5c8999
Vladimir Shturmer <cybersturmer@ya.ru>
2021-02-08 01:31:26 +0300

    Updated predefined host data.

--- src/.env.js
+++ src/.env.js
@@ -1,5 +1,5 @@
-export const DEBUG = true
+export const DEBUG = process.env && 'DEBUG' in process.env ? process.env.DEBUG : false
 
 export const DEBUG_ENV = 'https://localhost'
 
-export const PROD_ENV = 'http://localhost:8000'
+export const PROD_ENV = 'https://pmdragon.org'
