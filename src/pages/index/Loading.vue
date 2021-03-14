dadf10fb449a62c39aa1138c921b2237218b0d3f
Vladimir Shturmer <cybersturmer@ya.ru>
2021-03-14 19:38:13 +0300

    Let's do all requests at the same time by Promise.all

--- src/pages/index/Loading.vue
+++ src/pages/index/Loading.vue
@@ -15,13 +15,13 @@ export default {
   },
   computed: {
     loadingText () {
-      return `Loading ${this.module}...`
+      return this.$store.getters['current/LOADING_TEXT']
     }
   },
   methods: {
     showProgress () {
       this.$q.loading.show({
-        message: this.$store.getters['current/LOADING_TEXT'],
+        message: this.loadingText,
         spinner: QSpinnerGears
       })
     },
@@ -29,16 +29,50 @@ export default {
       this.$q.loading.hide()
     }
   },
+  beforeRouteLeave (to, from, next) {
+    this.$store.dispatch('current/START_LOADING', 'Loading data...')
+      .then(() =>
+        Promise.all([
+          this.$store.dispatch('auth/INIT_WORKSPACES'),
+          this.$store.dispatch('auth/INIT_PERSONS'),
+          this.$store.dispatch('core/INIT_ISSUE_STATES'),
+          this.$store.dispatch('core/INIT_ISSUE_TYPES'),
+          this.$store.dispatch('core/INIT_ISSUE_TYPE_ICONS'),
+          this.$store.dispatch('core/INIT_ISSUE_ESTIMATIONS'),
+          this.$store.dispatch('core/INIT_SPRINT_DURATIONS'),
+          this.$store.dispatch('core/INIT_ISSUES'),
+          this.$store.dispatch('core/INIT_ATTACHMENTS'),
+          this.$store.dispatch('core/INIT_SPRINTS'),
+          this.$store.dispatch('core/INIT_BACKLOGS'),
+          this.$store.dispatch('auth/INIT_INVITED')
+        ]))
+      .then(() => this.$store.dispatch('current/STOP_LOADING'))
+      .then(() => next())
+  },
   mounted () {
     this.showProgress()
-
-    if (!this.$store.getters['auth/IS_MY_DATA_FILLED']) {
-      return this.$router.push({ name: 'kickstart' })
-    } else {
-      this.$router.push({ name: 'workspaces' })
-    }
-
-    this.hideProgress()
+    this.$store.dispatch('current/START_LOADING', 'Loading data...')
+      .then(() =>
+        Promise.all([
+          this.$store.dispatch('auth/INIT_WORKSPACES'),
+          this.$store.dispatch('auth/INIT_PERSONS'),
+          this.$store.dispatch('core/INIT_ISSUE_STATES'),
+          this.$store.dispatch('core/INIT_ISSUE_TYPES'),
+          this.$store.dispatch('core/INIT_ISSUE_TYPE_ICONS'),
+          this.$store.dispatch('core/INIT_ISSUE_ESTIMATIONS'),
+          this.$store.dispatch('core/INIT_SPRINT_DURATIONS'),
+          this.$store.dispatch('core/INIT_ISSUES'),
+          this.$store.dispatch('core/INIT_ATTACHMENTS'),
+          this.$store.dispatch('core/INIT_SPRINTS'),
+          this.$store.dispatch('core/INIT_BACKLOGS'),
+          this.$store.dispatch('auth/INIT_INVITED')
+        ]))
+      .then(() => this.$store.dispatch('current/STOP_LOADING'))
+      .then(() => {
+        this.hideProgress()
+        if (!this.$store.getters['auth/IS_MY_DATA_FILLED']) this.$router.push({ name: 'kickstart' })
+        this.$router.push({ name: 'workspaces' })
+      })
   }
 }
 </script>
