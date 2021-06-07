9c91d3ed29586d0d2a02f1817d9046302a36819e
Vladimir Shturmer <cybersturmer@ya.ru>
2021-06-08 00:43:54 +0300

    Icons update for application.

---
+++ icongenie.sh
@@ -0,0 +1,5 @@
+# Generate profile for icons creation
+ icongenie profile --output  base --assets all --icon ./assets/logo.png background --include all --quality 10 --skip-trim --theme-color 393939 --svg-color 393939
+
+# Generate icons
+icongenie generate -p icongenie-base.json
