7d54ea34b464b40507e67680c734f8bfa57b86ea
Vladimir Shturmer <cybersturmer@ya.ru>
2021-06-12 17:57:04 +0300

    Let's move Issue CreatedAt and UpdatedAt to separate view.

---
+++ src/components/elements/issue_dialog/IssueCreateUpdateSection.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-card-section>
+    <!-- Readonly props such as created at and updated at -->
+    <q-input
+      dark
+      flat
+      square
+      readonly
+      :value="createdAt"
+      :mask="mask"
+      type="datetime"
+      label="Created at"
+      label-color="amber"
+    />
+    <q-input
+      dark
+      flat
+      square
+      readonly
+      :value="updatedAt"
+      :mask="mask"
+      type="datetime"
+      label="Updated at"
+      label-color="amber"
+    />
+  </q-card-section>
+</template>
+
+<script>
+import { DATETIME_MASK } from 'src/services/masks'
+import { date } from 'quasar'
+
+export default {
+	name: 'IssueCreateUpdateSection',
+	props: {
+	  issue: {
+	    type: Object,
+			required: true
+		}
+	},
+	computed: {
+	  mask () {
+	    return DATETIME_MASK
+		},
+		createdAt () {
+			return date.formatDate(this.issue.created_at, DATETIME_MASK)
+		},
+		updatedAt () {
+			return date.formatDate(this.issue.updated_at, DATETIME_MASK)
+		}
+	}
+}
+</script>
