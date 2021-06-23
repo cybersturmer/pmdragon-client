1c0091cd4e664078a0f4020ea899f071cabe606a
Vladimir Shturmer <cybersturmer@ya.ru>
2021-06-24 02:31:36 +0300

    About Dialog.

---
+++ src/components/dialogs/AboutDialog.vue
@@ -0,0 +1,88 @@
+<template>
+  <q-dialog ref="dialog" @hide="onDialogHide">
+    <q-card dark flat bordered class="q-dialog-plugin bg-secondary">
+      <q-card-section class="q-pb-none">
+				<p class="text-justify">
+					<a href="https://en.wikipedia.org/wiki/MIT_License" class="text-amber text-weight-bold">
+						MIT Licensed
+					</a> open-source project management tool to track tasks, epics, bugs, and manage it with agile boards and sprints.</p>
+      </q-card-section>
+			<q-card-actions vertical class="q-py-none">
+				<div class="text-h6 text-center q-pb-md">
+					<q-icon name="mdi-github" size="md"/>
+					GitHub
+				</div>
+				<q-list dark bordered separator>
+					<q-item clickable v-ripple>
+						<q-item-section>
+							<a href="https://github.com/cybersturmer/pmdragon" class="q-btn q-btn--flat">
+								Main repository
+							</a>
+						</q-item-section>
+					</q-item>
+					<q-item clickable v-ripple>
+						<q-item-section>
+							<a href="https://github.com/cybersturmer/pmdragon-core-api" class="q-btn q-btn--flat">
+								API repository
+							</a>
+						</q-item-section>
+					</q-item>
+					<q-item clickable v-ripple>
+						<q-item-section>
+							<a href="https://github.com/cybersturmer/pmdragon-client" class="q-btn q-btn--flat">
+								Client repository
+							</a>
+						</q-item-section>
+					</q-item>
+				</q-list>
+			</q-card-actions>
+			<q-card-section class="q-py-xs">
+				<div class="row">
+					<div class="col text-center">Copyright ©2020-present Vladimir Shturmer</div>
+				</div>
+				<div class="row">
+					<div class="col text-center">Version: v{{ version }}</div>
+				</div>
+			</q-card-section>
+			<q-card-actions vertical>
+				<q-btn
+					outline
+					color="amber"
+					label="Okay"
+					@click="onOKClick"/>
+			</q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+
+export default {
+	name: 'AboutDialog',
+	computed: {
+		version () {
+			return require('../../../package.json').version
+		}
+	},
+	methods: {
+		show () {
+			this.$refs.dialog.show()
+		},
+
+		hide () {
+			this.$refs.dialog.hide()
+		},
+
+		onDialogHide () {
+			this.$emit('hide')
+		},
+
+		onOKClick () {
+		},
+
+		onCancelClick () {
+			this.hide()
+		}
+	}
+}
+</script>
--- src/components/elements/ChangeConnectionElement.vue
+++ src/components/elements/ChangeConnectionElement.vue
@@ -12,12 +12,20 @@
         label="Set Host"
         text-color="amber"
         icon="mdi-connection" />
+			<q-fab-action
+				@click="showAboutDialog"
+				outline
+				class="q-pa-sm"
+				label="About"
+				text-color="amber"
+				icon="mdi-information" />
     </q-fab>
   </q-page-sticky>
 </template>
 
 <script>
 import ConnectionEditDialog from 'components/dialogs/ConnectionEditDialog.vue'
+import AboutDialog from '../dialogs/AboutDialog.vue'
 
 export default {
 	name: 'ChangeConnectionElement',
@@ -27,6 +35,13 @@ export default {
 				parent: this,
 				dark: true,
 				component: ConnectionEditDialog
+			})
+		},
+		showAboutDialog () {
+			this.$q.dialog({
+				parent: this,
+				dark: true,
+				component: AboutDialog
 			})
 		}
 	}
