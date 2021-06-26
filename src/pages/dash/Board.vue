cdad0dc010e3dd5579f829cf036051df61856881
Vladimir Shturmer <cybersturmer@ya.ru>
2021-06-27 02:57:03 +0300

    Small fix for View name

--- src/pages/dash/Board.vue
+++ src/pages/dash/Board.vue
@@ -118,7 +118,7 @@ import IssueBoard from 'components/elements/IssueBoard.vue'
 import { loading } from '../mixins/loading'
 
 export default {
-	name: 'BoardViewN',
+	name: 'BoardView',
 	components: {
 		NoStartedSprintNotification,
 		StartCompleteSprintButton,
@@ -177,11 +177,11 @@ export default {
 			/** We use it to show ex: 10 days remaining */
 			switch (true) {
 			case this.daysRemaining > this.daysAmount:
-				return 'Will start soon...'
+				return 'Will start...'
 			case this.daysRemaining > 0:
-				return `${this.daysRemaining} days`
+				return `${this.daysRemaining} days${this.$q.screen.gt.md ? ' remaining' : ''}`
 			case this.daysRemaining < 0:
-				return '0 days'
+				return `0 days${this.$q.screen.gt.md ? ' remaining' : ''}`
 			default:
 				throw new Error('Unexpected days remaining value')
 			}
