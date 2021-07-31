0ae4c7c79e4be522ce3102695980677cee731bc6
Vladimir Shturmer <cybersturmer@ya.ru>
2021-07-31 17:57:21 +0300

    Just Team refactored.

--- src/components/elements/ParticipantTeamMemberCard.vue
+++ src/components/elements/ParticipantTeamMemberCard.vue
@@ -1,46 +1,44 @@
 <template>
-	<div class="q-pa-xs col-xs-6 col-sm-6 col-md-3 col-lg-2 col-xl-2">
+	<div class="q-pa-xs col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-2">
 		<q-card
 		bordered>
-		<q-card-section horizontal>
-			<!-- Avatar block -->
-			<q-card-section class="flex flex-center">
-				<q-avatar v-if="person.avatar">
-					<img :src="person.avatar" :alt="person.username">
-				</q-avatar>
+			<q-card-section horizontal class="full-width">
+				<!-- Avatar block -->
+				<q-card-section class="col-3 flex flex-center">
+					<q-avatar v-if="person.avatar">
+						<img :src="person.avatar" :alt="person.username">
+					</q-avatar>
+				</q-card-section>
+				<!-- Name block -->
+				<q-card-section class="col-7 text-center">
+					<span class="block ellipsis">{{ person.first_name }} {{ person.last_name }}</span>
+					<span class="text-subtitle2">@{{ person.username }}</span>
+				</q-card-section>
+				<q-card-actions
+					v-if="!isMe && isOwner"
+					class="q-pa-none col-2">
+					<q-btn
+						flat
+						dense
+						icon="mdi-dots-vertical q-pa-none">
+						<q-menu
+							fit
+							anchor="bottom end"
+							self="top right"
+							auto-close
+							content-class="bg-negative text-white"
+						>
+							<q-list>
+								<q-item clickable
+												@click="$emit('removeTeamMember', person.id)">
+									<q-item-section>Remove from Workspace</q-item-section>
+								</q-item>
+							</q-list>
+						</q-menu>
+					</q-btn>
+				</q-card-actions>
 			</q-card-section>
-			<!-- Name block -->
-			<q-card-section class="text-center">
-				<span class="block">{{ person.first_name }}</span>
-				<span class="block">{{ person.last_name }}</span>
-				<span class="text-subtitle2">@{{ person.username }}</span>
-			</q-card-section>
-			<q-card-actions
-				v-if="!isMe && isOwner"
-				class="col-1 q-pa-none"
-			>
-				<q-btn
-					flat
-					dense
-					icon="mdi-dots-vertical q-pa-none">
-					<q-menu
-						fit
-						anchor="bottom end"
-						self="top right"
-						auto-close
-						content-class="bg-negative text-white"
-					>
-						<q-list>
-							<q-item clickable
-											@click="$emit('removeTeamMember', person.id)">
-								<q-item-section>Remove from Workspace</q-item-section>
-							</q-item>
-						</q-list>
-					</q-menu>
-				</q-btn>
-			</q-card-actions>
-		</q-card-section>
-	</q-card>
+		</q-card>
 	</div>
 </template>
 
