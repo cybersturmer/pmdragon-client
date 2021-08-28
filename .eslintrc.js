6ca9551612fc7fe7b0d1338ba076ac632d82cace
Vladimir Shturmer <cybersturmer@ya.ru>
2021-08-28 13:36:47 +0300

    Spaces.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,66 @@
 module.exports = {
-  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
-  // This option interrupts the configuration hierarchy at this file
-  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
-  root: true,
+	// https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+	// This option interrupts the configuration hierarchy at this file
+	// Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+	root: true,
 
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports,
-  },
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module' // Allows for the use of imports,
+	},
 
-  env: {
-    browser: true
-  },
+	env: {
+		browser: true
+	},
 
-  // Rules order is important, please avoid shuffling them
-  extends: [
-    // Base ESLint recommended rules
-    'eslint:recommended',
-    // See https://eslint.vuejs.org/rules/#available-rules
+	// Rules order is important, please avoid shuffling them
+	extends: [
+		// Base ESLint recommended rules
+		'eslint:recommended',
+		// See https://eslint.vuejs.org/rules/#available-rules
 		'plugin:vue/vue3-essential',
-    'standard'
-  ],
+		'standard'
+	],
 
-  plugins: [
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
-    // required to lint *.vue files
-    'vue',
+	plugins: [
+		// https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
+		// required to lint *.vue files
+		'vue'
 
-  ],
+	],
 
-  globals: {
-    ga: true, // Google Analytics
-    cordova: true,
-    __statics: true,
-    process: true,
-    Capacitor: true,
-    chrome: true
-  },
+	globals: {
+		ga: true, // Google Analytics
+		cordova: true,
+		__statics: true,
+		process: true,
+		Capacitor: true,
+		chrome: true
+	},
 
-  // add your custom rules here
-  rules: {
-    'indent': [2, 'tab'],
-    "no-mixed-spaces-and-tabs": [2, "smart-tabs"],
-    'no-tabs': 0,
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow paren-less arrow functions
-    'arrow-parens': 'off',
-    'one-var': 'off',
+	// add your custom rules here
+	rules: {
+		indent: [2, 'tab'],
+		'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
+		'no-tabs': 0,
+		// allow async-await
+		'generator-star-spacing': 'off',
+		// allow paren-less arrow functions
+		'arrow-parens': 'off',
+		'one-var': 'off',
 
-    'import/first': 'off',
-    'import/named': 'error',
-    'import/namespace': 'error',
-    'import/default': 'error',
-    'import/export': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'prefer-promise-reject-errors': 'off',
+		'import/first': 'off',
+		'import/named': 'error',
+		'import/namespace': 'error',
+		'import/default': 'error',
+		'import/export': 'error',
+		'import/extensions': 'off',
+		'import/no-unresolved': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'prefer-promise-reject-errors': 'off',
 
-
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+		// allow debugger during development only
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+	}
 }
