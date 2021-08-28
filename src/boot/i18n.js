04a0a92d57221fd36a3386452a8ea471c282b7f7
Vladimir Shturmer <cybersturmer@ya.ru>
2021-08-28 15:08:18 +0300

    Fallback and preventing warn in browser console.

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,14 +1,14 @@
 import { boot } from 'quasar/wrappers'
-import { createI18n } from 'vue-i18n'
+import { createI18n } from 'vue-i18n/index'
 import messages from 'src/i18n'
 
 const i18n = createI18n({
 	locale: 'en-US',
+	fallbackLocale: 'en-US',
 	messages
 })
 
 export default boot(({ app }) => {
-	// Set i18n instance on app
 	app.use(i18n)
 })
 
