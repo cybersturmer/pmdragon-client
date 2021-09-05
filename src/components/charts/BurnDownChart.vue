6fa008202eb5b509baf27e5fb59869ba64f49f05
Vladimir Shturmer <cybersturmer@ya.ru>
2021-09-06 00:16:08 +0300

    Not working variant for chart.

--- src/components/charts/BurnDownChart.vue
+++ src/components/charts/BurnDownChart.vue
@@ -2,7 +2,10 @@
 	<q-card>
 		<q-card-section class="column">
 			<div class="small_chart col self-center">
-				<LineChart />
+				<LineChart
+					v-if="isLoaded"
+					:chartdata="chartdata"
+					:options="options"/>
 			</div>
 		</q-card-section>
 	</q-card>
@@ -11,17 +14,150 @@
 <script>
 import { defineComponent } from 'vue'
 import LineChart from 'components/charts/LineChart'
+import { date, Dark } from 'quasar'
 
 export default defineComponent({
 	name: 'BurnDownChart',
 	components: {
 		LineChart
+	},
+	data: () => {
+		return {
+			isLoaded: false,
+			chartdata: [],
+			options: {
+				responsive: true,
+				aspectRatio: 0.5,
+				elements: {
+					line: {
+						tension: 0.1
+					}
+				},
+				legend: {
+					display: true,
+					labels: {
+						fontColor: Dark.isActive ? 'white' : 'black',
+						fontSize: 16,
+						padding: 16
+					}
+				},
+				tooltips: {
+					mode: 'point',
+					intersect: false
+				},
+				scales: {
+					xAxes: [{
+						gridLines: {
+							color: '#424242'
+						},
+						type: 'time',
+						distribution: 'series',
+						offset: true,
+						bounds: 'data',
+						time: {
+							parser: 'MM DD YYYY',
+							unit: 'day',
+							round: 'day',
+							tooltipFormat: 'll'
+						},
+						ticks: {
+							fontColor: Dark.isActive ? 'white' : 'black',
+							autoSkip: true
+						}
+					}],
+					yAxes: [{
+						display: true,
+						gridLines: {
+							color: '#424242'
+						},
+						scaleLabel: {
+							display: true,
+							labelString: 'Efforts',
+							fontColor: Dark.isActive ? 'white' : 'black',
+							fontSize: 14
+						},
+						ticks: {
+							fontColor: Dark.isActive ? 'white' : 'black',
+							autoSkip: true
+						}
+					}]
+				}
+			}
+		}
+	},
+	async mounted () {
+		const sprint = this.$store.getters['core/SPRINT_STARTED_BY_CURRENT_PROJECT']
+
+		const sprintStartedAt = new Date(sprint.started_at)
+		const sprintFinishedAt = new Date(sprint.finished_at)
+		const diffDays = date
+			.getDateDiff(sprintFinishedAt, sprintStartedAt, 'days')
+
+		const totalSP = this.$store.getters['core/STORY_POINT_TOTAL_FOR_STARTED_SPRINT']
+		const perDayDecrement = totalSP / diffDays
+
+		const estimations = await this.$http
+			.auth(true)
+			.expect(200)
+			.get(
+				`/core/sprint-estimations/?sprint=${sprint.id}`
+			)
+
+		const daysLabels = []
+		const expectedTimeValues = []
+		const realTimeValues = []
+
+		for (const datum of estimations.data) {
+			realTimeValues.push({
+				x: new Date(datum.point_at),
+				y: datum.estimated_value
+			})
+		}
+
+		for (let i = diffDays; i >= 0; i--) {
+			const dayNumber = diffDays - i
+			const _date = date.addToDate(sprintStartedAt, { days: dayNumber })
+			const _value = Math.round(totalSP - perDayDecrement * dayNumber)
+			expectedTimeValues.push({
+				x: _date,
+				y: _value
+			})
+		}
+
+		this.chartdata = {
+			labels: daysLabels,
+			datasets: [
+				{
+					label: 'Estimated Effort',
+					fill: false,
+					pointRadius: 3,
+					borderColor: 'rgba(255,255,255,0.85)',
+					borderWidth: 3,
+					data: expectedTimeValues,
+					xAxisID: 'xAxis'
+				},
+				{
+					label: 'Actual Effort',
+					fill: false,
+					pointRadius: 3,
+					borderColor: '#4c8ef1',
+					borderWidth: 3,
+					data: realTimeValues,
+					yAxisID: 'yAxis'
+				}
+			]
+		}
+
+		this.isLoaded = true
 	}
+
 })
 </script>
 <style>
 .small_chart {
 	width: 600px;
 	height: 600px;
+	max-height: 500px;
+	max-width: 500px;
 }
 </style>
--- src/components/charts/LineChart.vue
+++ src/components/charts/LineChart.vue
@@ -1,86 +1,45 @@
 <template>
-	<canvas id="line_chart" width="600" height="600"></canvas>
+	<canvas id="line_chart" width="500" height="500"></canvas>
 </template>
 <script>
 import { defineComponent } from 'vue'
 import {
 	Chart,
-	BarController,
 	LinearScale,
-	CategoryScale,
-	BarElement
+	LineController,
+	LineElement,
+	PointElement,
+	CategoryScale
 } from 'chart.js'
 
 export default defineComponent({
 	name: 'LineChart',
-	data () {
-		return {
-			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-			datasets: [{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
-				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
-				],
-				borderWidth: 1
-			}]
+	props: {
+		chartdata: {
+			type: Object,
+			required: true
+		},
+		options: {
+			type: Object,
+			required: true
 		}
 	},
 	mounted () {
 		const ctx = document.getElementById('line_chart').getContext('2d')
+
 		Chart.register(
 			LinearScale,
-			BarController,
+			LineController,
 			CategoryScale,
-			BarElement
+			LineElement,
+			PointElement
 		)
+
 		// eslint-disable-next-line no-unused-vars
 		const myChart = new Chart(ctx, {
-			type: 'bar',
-			data: {
-				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-				datasets: [{
-					label: '# of Votes',
-					data: [12, 19, 3, 5, 2, 3],
-					backgroundColor: [
-						'rgba(255, 99, 132, 0.2)',
-						'rgba(54, 162, 235, 0.2)',
-						'rgba(255, 206, 86, 0.2)',
-						'rgba(75, 192, 192, 0.2)',
-						'rgba(153, 102, 255, 0.2)',
-						'rgba(255, 159, 64, 0.2)'
-					],
-					borderColor: [
-						'rgba(255, 99, 132, 1)',
-						'rgba(54, 162, 235, 1)',
-						'rgba(255, 206, 86, 1)',
-						'rgba(75, 192, 192, 1)',
-						'rgba(153, 102, 255, 1)',
-						'rgba(255, 159, 64, 1)'
-					],
-					borderWidth: 1
-				}]
-			},
-			options: {
-				scales: {
-					y: {
-						beginAtZero: true
-					}
-				}
-			}
+			type: 'line',
+			data: this.chartdata,
+			options: this.options
 		})
 	}
 })
