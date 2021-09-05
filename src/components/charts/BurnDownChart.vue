52e83ce212c9d7502ccf259dd252f71f080f5b7d
Vladimir Shturmer <cybersturmer@ya.ru>
2021-09-05 20:38:55 +0300

    Unfortunately all chart.js wrappers for vue3 were terrible. So we have to write our own component to use chart.

--- src/components/charts/BurnDownChart.vue
+++ src/components/charts/BurnDownChart.vue
@@ -1,158 +1,27 @@
 <template>
-  <q-card>
-    <q-card-section>
-      <div class="small_chart">
-      <LineChart
-        v-if="isLoaded"
-        :chartdata="chartdata"
-        :options="options"
-      />
-      </div>
-    </q-card-section>
-  </q-card>
+	<q-card>
+		<q-card-section class="column">
+			<div class="small_chart col self-center">
+				<LineChart />
+			</div>
+		</q-card-section>
+	</q-card>
 </template>
 
 <script>
-import LineChart from 'src/components/charts/LineChart'
-import { date, Dark } from 'quasar'
+import { defineComponent } from 'vue'
+import LineChart from 'components/charts/LineChart'
 
-export default {
+export default defineComponent({
 	name: 'BurnDownChart',
 	components: {
 		LineChart
-	},
-	data: () => ({
-		isLoaded: false,
-		chartdata: null,
-		options: {
-			responsive: true,
-			aspectRatio: 0.5,
-			elements: {
-				line: {
-					tension: 0.1
-				}
-			},
-			legend: {
-				display: true,
-				labels: {
-					fontColor: Dark.isActive ? 'white' : 'black',
-					fontSize: 16,
-					padding: 16
-				}
-			},
-			tooltips: {
-				mode: 'point',
-				intersect: false
-			},
-			scales: {
-				xAxes: [{
-					gridLines: {
-						color: '#424242'
-					},
-					type: 'time',
-					distribution: 'series',
-					offset: true,
-					bounds: 'data',
-					time: {
-						parser: 'MM DD YYYY',
-						unit: 'day',
-						round: 'day',
-						tooltipFormat: 'll'
-					},
-					ticks: {
-						fontColor: Dark.isActive ? 'white' : 'black',
-						autoSkip: true
-					}
-				}],
-				yAxes: [{
-					display: true,
-					gridLines: {
-						color: '#424242'
-					},
-					scaleLabel: {
-						display: true,
-						labelString: 'Efforts',
-						fontColor: Dark.isActive ? 'white' : 'black',
-						fontSize: 14
-					},
-					ticks: {
-						fontColor: Dark.isActive ? 'white' : 'black',
-						autoSkip: true
-					}
-				}]
-			}
-		}
-	}),
-	async mounted () {
-		this.isLoaded = false
-		const sprint = this.$store.getters['core/SPRINT_STARTED_BY_CURRENT_PROJECT']
-
-		const sprintStartedAt = new Date(sprint.started_at)
-		const sprintFinishedAt = new Date(sprint.finished_at)
-		const diffDays = date
-			.getDateDiff(sprintFinishedAt, sprintStartedAt, 'days')
-
-		const totalValue = this.$store.getters['core/STORY_POINT_TOTAL_FOR_STARTED_SPRINT']
-		const perDayDecrement = totalValue / diffDays
-
-		const estimations =
-			await this.$http
-				.auth(true)
-				.expect(200)
-				.get(`/core/sprint-estimations/?sprint=${sprint.id}`)
-
-		const daysLabels = []
-		const expectedTimeValues = []
-		const realTimeValues = []
-
-		for (const datum of estimations.data) {
-			realTimeValues.push({
-				x: new Date(datum.point_at),
-				y: datum.estimated_value
-			})
-		}
-
-		for (let i = diffDays; i >= 0; i--) {
-			const dayNumber = diffDays - i
-			const _date = date.addToDate(sprintStartedAt, { days: dayNumber })
-			const _value = Math.round(totalValue - perDayDecrement * dayNumber)
-			expectedTimeValues.push({
-				x: _date,
-				y: _value
-			})
-		}
-
-		this.chartdata = {
-			labels: daysLabels,
-			datasets: [
-				{
-					label: 'Estimated Effort',
-					fill: false,
-					pointRadius: 3,
-					borderColor: Dark.isActive ? 'white' : 'red',
-					borderWidth: 3,
-					data: expectedTimeValues
-				},
-				{
-					label: 'Actual Effort',
-					fill: false,
-					pointRadius: 3,
-					borderColor: '#4c8ef1',
-					borderWidth: 3,
-					data: realTimeValues
-				}
-			]
-		}
-
-		this.isLoaded = true
 	}
-}
+})
 </script>
-
-<style scoped>
-  .small_chart {
-    position: relative;
-    max-width: 700px;
-    margin: 0 auto;
-  }
+<style>
+.small_chart {
+	width: 600px;
+	height: 600px;
+}
 </style>
--- src/components/charts/LineChart.vue
+++ src/components/charts/LineChart.vue
@@ -1,20 +1,87 @@
+<template>
+	<canvas id="line_chart" width="600" height="600"></canvas>
+</template>
 <script>
-import { Line } from 'vue-chartjs'
+import { defineComponent } from 'vue'
+import {
+	Chart,
+	BarController,
+	LinearScale,
+	CategoryScale,
+	BarElement
+} from 'chart.js'
 
-export default {
-	extends: Line,
-	props: {
-		chartdata: {
-			type: Object,
-			default: null
-		},
-		options: {
-			type: Object,
-			default: null
+export default defineComponent({
+	name: 'LineChart',
+	data () {
+		return {
+			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
 		}
 	},
 	mounted () {
-		this.renderChart(this.chartdata, this.options)
+		const ctx = document.getElementById('line_chart').getContext('2d')
+		Chart.register(
+			LinearScale,
+			BarController,
+			CategoryScale,
+			BarElement
+		)
+		// eslint-disable-next-line no-unused-vars
+		const myChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				datasets: [{
+					label: '# of Votes',
+					data: [12, 19, 3, 5, 2, 3],
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					y: {
+						beginAtZero: true
+					}
+				}
+			}
+		})
 	}
-}
+})
 </script>
