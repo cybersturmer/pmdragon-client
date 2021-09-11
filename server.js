05ba4c4e5e47122f45f41db3eca3ee7e5cfd472e
Vladimir Shturmer <cybersturmer@ya.ru>
2021-09-11 22:37:38 +0300

    Refactored. min.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  port = process && process.env && process.env.PORT ? process.env.PORT : 80
+	express = require('express'),
+	serveStatic = require('serve-static'),
+	history = require('connect-history-api-fallback'),
+	port = process && process.env && process.env.PORT ? process.env.PORT : 80
 
 const app = express()
 const path = require('path')
