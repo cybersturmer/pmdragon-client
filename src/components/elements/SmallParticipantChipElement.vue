352dc078861a31294fd988d4ffad11e8956a874f
Vladimir Shturmer <cybersturmer@ya.ru>
2021-10-06 22:31:16 +0300

    Small participants show username if first name and | or last name were not defined.

--- src/components/elements/SmallParticipantChipElement.vue
+++ src/components/elements/SmallParticipantChipElement.vue
@@ -4,17 +4,17 @@
 		:text-color="`${$q.dark.isActive ? 'accent' : 'dark'}`"
 		square
     :key="participant.id">
-    <q-avatar v-if="participant.avatar">
+    <q-avatar v-if="avatar">
       <img
-        :src="participant.avatar"
-        :alt="`${participant.first_name} ${participant.last_name}`">
+        :src="avatar"
+        :alt="title">
     </q-avatar>
-		<span class="text-bold">{{ participant.first_name }} {{ participant.last_name }}</span>
+		<span class="text-bold">{{ nameIsDefined ? title : username }}</span>
   </q-chip>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
 
 export default defineComponent({
 	name: 'SmallParticipantChipElement',
@@ -22,6 +22,30 @@ export default defineComponent({
 		participant: {
 			type: Object,
 			required: true
+		}
+	},
+	setup (props, context) {
+		const nameIsDefined = computed(() => {
+			return Boolean(props.participant.first_name) && Boolean(props.participant.last_name)
+		})
+
+		const title = computed(() => {
+			return `${props.participant.first_name} ${props.participant.last_name}`
+		})
+
+		const username = computed(() => {
+			return props.participant.username
+		})
+
+		const avatar = computed(() => {
+			return props.participant.avatar
+		})
+
+		return {
+			avatar,
+			title,
+			username,
+			nameIsDefined
 		}
 	}
 })
