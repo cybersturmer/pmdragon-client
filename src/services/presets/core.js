38bda3aa7afbec05664061491a8d8cd908e30b03
Vladimir Shturmer <cybersturmer@ya.ru>
2021-10-15 21:24:57 +0300

    2 more presets.

--- src/services/presets/core.js
+++ src/services/presets/core.js
@@ -4,3 +4,17 @@ export const issueOrdering = {
 	method: 'put',
 	endpoint: '/core/issue/ordering/'
 }
+
+export const getSprintRemainingPreset = {
+	auth: true,
+	expect: 200,
+	method: 'get',
+	endpoint: '/core/sprint-efforts-history/?sprint=<placeholder:sprint.id>'
+}
+
+export const getSprintGuidelinePreset = {
+	auth: true,
+	expect: 200,
+	method: 'get',
+	endpoint: '/core/sprint-guideline/<placeholder:sprint.id>/'
+}
