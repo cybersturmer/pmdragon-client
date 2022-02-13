310931c2133a047fbf01b436dbc82feabc304175
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-14 02:21:33 +0300

    App mentioning removed.

--- src/boot/capacitor.js
+++ src/boot/capacitor.js
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers'
 import * as Sentry from '@sentry/capacitor'
 
-export default boot(({ app }) => {
+export default boot(() => {
 	Sentry.init({
 		dsn: 'https://8410b5d5deb2408597d0e08c2a5cbeb1@o514097.ingest.sentry.io/6202017',
 		release: 'pmdragon-mobile-application',
